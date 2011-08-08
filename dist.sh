6b85ecb9def35526f2f43c53a392dd8fbc6aef49
aisensiy <aisensiy@163.com>
2011-08-08 21:12:36 +0800

    I dont know why the manifest.json file become a binary file at the last version. Now I correct it and git the dist.sh new function to zip the dist files where run it. 	modified:   dist.sh 	modified:   manifest.json

--- dist.sh
+++ dist.sh
@@ -11,3 +11,5 @@ echo $distdir"/"
 rm -r $distdir
 mkdir $distdir
 cp -r background.html popup.html manifest.json css/ js/ icon.png icon48.png $distdir'/'
+rm domain_time_tracker.zip
+cd $distdir && zip -r domain_time_tracker.zip * && mv domain_time_tracker.zip ../
